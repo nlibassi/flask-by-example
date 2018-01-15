a613edff2d812eb79bb016194738437f2f93f524
Nick <nlibassi@gmail.com>
2018-01-15 21:29:30 +0300

    added nltk.txt to root with 'punkt' as text

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def count_and_save_words(url):
 
     # text processing
     raw = BeautifulSoup(r.text).get_text()
-    nltk.data.path.append('/home/nlibassi/flask-by-example/nltk_data/tokenizers/punkt/PY3/') # set the path (how is this done on heroku?)
+    nltk.data.path.append('/home/nlibassi/flask-by-example/nltk_data/') # set the path (how is this done on heroku?)
     tokens = nltk.word_tokenize(raw) # returns list, same as split using space?
     text = nltk.Text(tokens)
 
